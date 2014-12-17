e8c650813340e0e09992f145d167a1aa8ef064af
nrasheed <nrasheed@accertify.com>
2014-12-17 11:19:47 -0600

    update

--- datapull/pull.py
+++ datapull/pull.py
@@ -1,5 +1,38 @@
 #!/usr/bin/python
 
-with open("ayhp_page_source.html","r") as f:
-  for line in f:
-    print line
+from HTMLParser import HTMLParser
+from lxml import html
+
+class MyHTMLParser(HTMLParser):
+    def __init__(self, html_snippet):
+        HTMLParser.__init__(self)
+        self.comments = []
+        self.feed(html_snippet)
+    def handle_comment(self, data):
+        self.comments.append(data)
+    def get_comments(self):
+        return self.comments
+
+def extract_status_snippet(html_snippet):
+    tree = html.fromstring(html_snippet)
+    snippet = tree.xpath('//span[@class="userContent"]/text()')
+    return snippet
+        
+#f = open("ayhp_page_source_formatted.html")
+f = open("ayhp_page_source.html")
+response = f.read()
+myparser = MyHTMLParser(response)
+f.close()
+comments = myparser.get_comments()
+for comment in comments:
+    print 'comment', comment
+
+print 'found',len(comments),'comments'
+
+for comment in comments:
+    statuses = extract_status_snippet(comment)
+    if statuses:
+        for status in statuses:
+            print 'status', status
+
+print 'done'
